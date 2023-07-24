8f7214ede4b07b72bc25cba38d7964e6b9da9dc1
Kamil Wilim <kamil.wilim@devopsi.pl>
2023-07-24 16:55:08 +0200

    better

--- app/contact/page.js
+++ app/contact/page.js
@@ -47,7 +47,7 @@ export default function Home() {
         </p>
       </section>
 
-      <Footer />
+      <Footer wave={false} />
     </>
   );
 }
--- components/templates/Navbar.js
+++ components/templates/Navbar.js
@@ -1,8 +1,14 @@
 /* eslint-disable @next/next/no-img-element */
+"use client";
+
+import { useState } from "react";
+
 export default function Navbar(props) {
+  const [navbarOpen, setNavbarOpen] = useState(false);
+
   return (
     <nav className={props.background === true ? "bg-slate-800" : ""}>
-      <ul className="flex justify-center align-middle items-center p-6 max-w-6xl mx-auto">
+      <ul className="flex justify-between md:justify-center align-middle items-center p-6 max-w-6xl mx-auto">
         <li className="md:grow px-5 text-xl font-bold">
           <a href="/">
             <img
@@ -11,6 +17,27 @@ export default function Navbar(props) {
               alt="My Happy SVG"
             />
           </a>
+        </li>
+        <li className="md:hidden">
+          <svg
+            onClick={() => {
+              setNavbarOpen(!navbarOpen);
+              document.body.style.overflow = "hidden";
+            }}
+            className="w-6 h-6 text-white"
+            aria-hidden="true"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 17 14"
+          >
+            <path
+              stroke="currentColor"
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              stroke-width="2"
+              d="M1 1h15M1 7h15M1 13h15"
+            />
+          </svg>
         </li>
         <li className="hidden md:block px-5 text-white text-2xl font-bold hover:underline">
           Blog
@@ -24,6 +51,47 @@ export default function Navbar(props) {
           </button>
         </li>
       </ul>
+      {navbarOpen === true ? (
+        <section className="bg-slate-800 text-white absolute top-0 w-screen h-screen z-50 ">
+          <div className="flex flex-row justify-end pr-6 pt-6">
+            <svg
+              onClick={() => {
+                setNavbarOpen(!navbarOpen);
+                document.body.style.overflow = "auto";
+              }}
+              className="w-6 h-6 text-white"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 17 14"
+            >
+              <path
+                stroke="currentColor"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+                stroke-width="2"
+                d="M1 1h15M1 7h15M1 13h15"
+              />
+            </svg>
+          </div>
+          <div className="flex mt-20 flex-col">
+            <a href="/" className="m-auto mt-8 font-bold text-2xl">
+              Home Page
+            </a>
+            <a
+              href="/futurology"
+              className="m-auto mt-8 hover:underline font-bold text-2xl"
+            >
+              Futurology
+            </a>
+            <p className="m-auto mt-8">
+              <button className="bg-slate-700 hover:bg-slate-950 text-white font-bold py-2 px-4 rounded-full text-3xl">
+                <a href="/contact">Contact</a>
+              </button>
+            </p>
+          </div>
+        </section>
+      ) : null}
     </nav>
   );
 }
