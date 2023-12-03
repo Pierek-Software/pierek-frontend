4820d949e0eaac48332fa9cf1331035eed1c74bf
Kamil Wilim <kamilwilim98@gmail.com>
2023-12-03 13:30:55 +0700

    icon update

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,7 +11,25 @@ export default function Document() {
           crossOrigin="anonymous"
           referrerPolicy="no-referrer"
         />
-        <link rel="icon" href="/favicon.ico" />
+
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/favicon-16x16.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
 
         <script
           async
